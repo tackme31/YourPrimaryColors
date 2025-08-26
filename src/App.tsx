7aee5921890c9a9cd8c35af813c20f96bc14acbe
Takumi Yamada <xirtardauq@gmail.com>
2025-08-26 21:07:10 +0900

    add react-color

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
 import { useState } from "react";
+import { SketchPicker } from "react-color";
 import "./App.css";
 
 function App() {
@@ -6,6 +7,7 @@ function App() {
 
   return (
     <>
+      <SketchPicker />
       <div></div>
     </>
   );
